b07562231b146e3e9eee8513d5c0ec42cd7fa4d7
Mohammad Tahvildary <shamot.group@gmail.com>
2025-02-05 00:31:15 +0400

    feat(api-docs): upgrade NestJS dependencies and integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import * as dotenv from 'dotenv-safe';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Devotel Assignment API')
+        .setContact(
+            'Mohammad Tahvildary',
+            'https://itsmrtech.com',
+            'mohammad.tahvildary@gmail.com'
+        )
+        .build();
+    const document = SwaggerModule.createDocument(app, config, {});
+    SwaggerModule.setup('docs', app, document);
     await app.listen(3000);
 }
 bootstrap();

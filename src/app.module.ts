e874c075d3c2cc28fe2820449b4302a7986df12e
Mohammad Tahvildary <shamot.group@gmail.com>
2025-02-04 15:33:48 +0400

    chore(env): update environment variable provider and add database connection
    - ConfigModule added instead of dotenv config
    - Postgresql connection added to app module
    - Joi added for .env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        EXTERNAL_API_URL_1: Joi.string().uri().required(),
+        EXTERNAL_API_URL_2: Joi.string().uri().required(),
+        CRON_SCHEDULE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
